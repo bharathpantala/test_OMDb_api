d650dc4ace64a2e307b8e217b86d4993316d9cac
Bharath Pantala <bharathp@hcl.com>
2019-04-14 06:42:49 +0000

    1.0

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+# Removing old image
+
+
+
+sudo docker rmi  -f my_omdb
+# Build the docker image.
+sudo docker build -t my_omdb .
+
+
+
+
+
+RUNNING=$(sudo docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+if [$? -eq 1 ]; then
+		echo "'$CONTAINER' does not exist."
+	else
+			sudo docker rm -f $CONTAINER
+		fi
+		      # run  your container
+		            echo ""
+			          echo " -------- Deployment Phase Started :: Building Docker container :: ......"
+				        sudo docker run -d -p 80:80 --name OMDb1 my_omdb:latest
+
+					#-Completion 
+					echo "----------------------------------------"
+					echo "View App deployed here " http://www.bharathpantala.cf/
+
+
+
+
