4c02aae5922e2b629f69dc8c95796ba9b399b2ca
Bharath Pantala <bharathpantala@mail.com>
2019-04-15 09:18:48 +0000

    3.0
    Yellow

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@
 ########################################################################################
 
 # Removing old image
-sudo docker rmi  -f my_omdb
+sudo docker rmi  -f my_omdb:latest
 # Build the docker image.
-sudo docker build -t my_omdb .
+sudo docker build -t my_omdb:latest .
 # Removing an old Running Container.
     
 RUNNING=$(sudo docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
 if [$? -eq 1 ]; then
 		echo "'$CONTAINER' does not exist."
 	else
-			sudo docker rm -f $CONTAINER
+			sudo docker rm -f OMDb1
 		fi
 		      # run  your container
 		            echo ""
@@ -22,7 +22,7 @@ if [$? -eq 1 ]; then
 
 					#-Completion 
 					echo "----------------------------------------"
-					echo "View App deployed here " http://www.bharathpantala.cf/
+					echo "View App deployed here " http://localhost or ip /
 
 
 
