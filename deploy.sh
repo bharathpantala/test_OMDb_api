028ad24f4f32c5b6e936b9265a3bc7804009e013
Bharath Pantala <bharathpantala@gmail.com>
2019-04-15 06:08:13 +0530

    1.1
    Modified deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
+#!bin/bash
+########################################################################################
+##################################  Deployment Script ####################################
+########################################################################################
+
 # Removing old image
-
-
-
 sudo docker rmi  -f my_omdb
 # Build the docker image.
 sudo docker build -t my_omdb .
-
-
-
-
-
+# Removing an old Running Container.
+    
 RUNNING=$(sudo docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
 if [$? -eq 1 ]; then
 		echo "'$CONTAINER' does not exist."
